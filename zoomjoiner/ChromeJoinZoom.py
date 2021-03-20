8150ebe72b9c5c3f2d2f0ac6cc3a576fb42a0b12
NKim123 <67401402+NKim123@users.noreply.github.com>
2021-03-20 19:27:30 -0400

    15

--- zoomjoiner/ChromeJoinZoom.py
+++ zoomjoiner/ChromeJoinZoom.py
@@ -2,55 +2,52 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import pyautogui
-
+import cv2
+import os
 import time
 
-def JoinZoom(link):
-    username = "244388@mcpsmd.net"
-    password = "asdfghjkl;'"
-    browser = webdriver.Chrome("/Users/noahkim/Documents/chromedriver")
-    #chrome_options = webdriver.ChromeOptions()
-    #chrome_options.add_argument("--disable-default-apps")
-    #browser = webdriver.Chrome("/Users/noahkim/Documents/chromedriver", options=chrome_options)
+def JoinZoom(link, username, password):
 
-    #email
+    currentFolder = os.getcwd()
+    browser = webdriver.Chrome(f"{currentFolder}/chromedriver")
+
+    # email
     browser.get("https://accounts.google.com/Login")
     usernameBox = browser.find_element_by_css_selector("#identifierId")
     usernameBox.send_keys(username)
     usernameBox.send_keys(Keys.ENTER)
-    time.sleep(2)
-    #password
+    time.sleep(5)
+    # password
     passwordBox = browser.find_element_by_css_selector("#password > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)")
     passwordBox.send_keys(password)
     passwordBox.send_keys(Keys.ENTER)
-    #time.sleep(2)
-    #opening signin
+    # opening signin
     browser.execute_script("window.open('');")
     browser.switch_to.window(browser.window_handles[1])
-    #time.sleep(2)
     browser.get("https://zoom.us")
     browser.find_element_by_css_selector(".link").click()
-    #time.sleep(2)
     browser.find_element_by_css_selector("a.login-btn:nth-child(3)").click()
-    #joining the meeting
+    # joining the meeting
     browser.execute_script("window.open('');")
     browser.switch_to.window(browser.window_handles[2])
     browser.get(link)
-    
     time.sleep(2)
-    print("something")
-    buttonLoc = pyautogui.locateOnScreen('openzoom.png')
-    print(buttonLoc)
-    print("something2")
-    pyautogui.Click(buttonLoc)
+    start = time.time()
+    time.sleep(3)
+    while True:
+        var = pyautogui.locateOnScreen('open.png', confidence=0.6)
+        if var != None:
+            pyautogui.click(var)
+            break
+        var = pyautogui.locateOnScreen('openzoom.png', confidence=0.6)
+        if var != None:
+            pyautogui.click(var)
+            break
+        elif (time.time() - start) >= 120:
+            print("link " + link + " not opened")
+            break
+        time.sleep(3)
+    return
 
-    '''
-    I want it to accept the dialogue box here
-    '''
 
-    #this is just to keep the window open for now
-    while(True):
-       pass
-
-
-JoinZoom("https://mcpsmd.zoom.us/j/97130321813?pwd=cW9tYU9wVjV5c3hKbEZsU0FwalAxUT09")
+JoinZoom("https://mcpsmd.zoom.us/j/97130321813?pwd=cW9tYU9wVjV5c3hKbEZsU0FwalAxUT09", "244388@mcpsmd.net", "asdfghjkl;'")
