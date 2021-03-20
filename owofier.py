1a06f27587149e633f15b99fc2613a4979229845
NKim123 <67401402+NKim123@users.noreply.github.com>
2021-03-19 23:05:11 -0400

    5

--- owofier.py
+++ owofier.py
@@ -7,7 +7,8 @@ def owofier(message):
         rIndex = returnMessage.find("r")
         lIndex = returnMessage.find("l")
         theIndex = returnMessage.find("the")
-        IIndex = returnMessage.find("I")
+        TheIndex = returnMessage.find("The")
+        IIndex = returnMessage.find("I ")
         owoUwu = random.randint(0,1)
         if rIndex != -1:
             returnMessage = returnMessage[0:rIndex] + "w" + returnMessage[rIndex+1:]
@@ -15,6 +16,8 @@ def owofier(message):
             returnMessage = returnMessage[0:lIndex] + "w" + returnMessage[lIndex+1:]
         if theIndex != -1:
             returnMessage = returnMessage[0:theIndex] + "da" + returnMessage[theIndex+3:]
+        if TheIndex != -1:
+            returnMessage = returnMessage[0:TheIndex] + "Da" + returnMessage[TheIndex+3:]
         if IIndex != -1:
             returnMessage = returnMessage[0:IIndex] + "me" + returnMessage[IIndex+1:]
         if rIndex == -1 and lIndex == -1 and theIndex == -1 and IIndex == -1:
@@ -27,4 +30,4 @@ def owofier(message):
 
     return returnMessage
 
-print(owofier("My team and I are building an organizational/grades app for students. I was wondering if you would be interested in joining a call with my and my teammates to give us some insight based on your past experiences as a teacher."))
+
