1340019a6ffa3df2153a0478ffbc2ccc9be74741
NKim123 <67401402+NKim123@users.noreply.github.com>
2021-03-19 22:48:54 -0400

    5

--- owofier.py
+++ owofier.py
@@ -1,26 +1,30 @@
+import random
 
 def owofier(message):
     allChanged = False
     returnMessage = message
     while not allChanged:
-        rIndex = message.find("r")
-        lIndex = message.find("l")
-        thIndex = message.find("th")
-        theIndex = message.find(" the ")
-        IIndex = message.find(" I ")
+        rIndex = returnMessage.find("r")
+        lIndex = returnMessage.find("l")
+        theIndex = returnMessage.find("the")
+        IIndex = returnMessage.find("I")
+        owoUwu = random.randint(0,1)
         if rIndex != -1:
             returnMessage = returnMessage[0:rIndex] + "w" + returnMessage[rIndex+1:]
         if lIndex != -1:
             returnMessage = returnMessage[0:lIndex] + "w" + returnMessage[lIndex+1:]
-        if thIndex != -1:
-            returnMessage = returnMessage[0:thIndex] + "w" + returnMessage[thIndex+1:]
         if theIndex != -1:
-            returnMessage = returnMessage[0:theIndex] + "w" + returnMessage[theIndex+1:]
+            returnMessage = returnMessage[0:theIndex] + "da" + returnMessage[theIndex+3:]
         if IIndex != -1:
-            returnMessage = returnMessage[0:IIndex] + "w" + returnMessage[IIndex+1:]
-        if rIndex == -1 and lIndex == -1 and thIndex == -1 and theIndex == -1 and IIndex == -1:
+            returnMessage = returnMessage[0:IIndex] + "me" + returnMessage[IIndex+1:]
+        if rIndex == -1 and lIndex == -1 and theIndex == -1 and IIndex == -1:
             allChanged = True
-        print("once")
+
+    if owoUwu == 1:
+        returnMessage += " owo >:3"
+    elif owoUwu == 0:
+        returnMessage += " uwu :3"
+
     return returnMessage
 
-owofier("46th President of the United States, husband to @FLOTUS, proud dad & pop. Tweets may be archived")
+print(owofier("My team and I are building an organizational/grades app for students. I was wondering if you would be interested in joining a call with my and my teammates to give us some insight based on your past experiences as a teacher."))
