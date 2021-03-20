a98c5211c670138714dc9881584df183f00590ff
chaifam <justin.by.chai@gmail.com>
2021-03-20 14:55:23 -0400

    Update Startup.py

---
+++ zoomjoiner/Startup.py
@@ -0,0 +1,10 @@
+from flask import Flask, redirect, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("JoinPage.html")
+
+if __name__=="__main__":
+    app.run()
