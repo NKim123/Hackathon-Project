29db4205918a969ca3bb44e62064cfdf9d1fafce
noah kim <67401402+NKim123@users.noreply.github.com>
2021-03-21 09:43:18 -0400

    Update ChromeJoinZoom.py

--- zoomjoiner/attempt1/ChromeJoinZoom.py
+++ zoomjoiner/attempt1/ChromeJoinZoom.py
@@ -9,39 +9,50 @@ import time
 def JoinZoom(link, username, password):
 
     currentFolder = os.getcwd()
-    print(currentFolder)
-    print("HERE\n\n\n\n\n")
-    browser = webdriver.Chrome(f"{currentFolder}/chromedriver")
+    optionss = webdriver.ChromeOptions()
+    optionss.add_argument('controlled')
+    optionss.add_argument('disable-notifications')
+    optionss.add_experimental_option("excludeSwitches", ['enable-automation'])
+    
+    browser = webdriver.Chrome("C:/Users/noahk/Documents/GitHub/Hackathon-Project/zoomjoiner/attempt1/chromedriver", options=optionss)
 
-    # email
-    browser.get("https://accounts.google.com/Login")
-    usernameBox = browser.find_element_by_css_selector("#identifierId")
-    usernameBox.send_keys(username)
-    usernameBox.send_keys(Keys.ENTER)
-    time.sleep(5)
-    # password
-    passwordBox = browser.find_element_by_css_selector("#password > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)")
-    passwordBox.send_keys(password)
-    passwordBox.send_keys(Keys.ENTER)
     # opening signin
-    browser.execute_script("window.open('');")
-    browser.switch_to.window(browser.window_handles[1])
-    browser.get("https://zoom.us")
-    browser.find_element_by_css_selector(".link").click()
-    browser.find_element_by_css_selector("a.login-btn:nth-child(3)").click()
+    try:
+        browser.get("https://zoom.us")
+        browser.find_element_by_css_selector(".link").click()
+        browser.find_element_by_css_selector("a.login-btn:nth-child(2)").click()
+    # email
+    except:
+        pass
+
+    while True:
+        try:
+            browser.get("https://accounts.google.com/Login")
+            usernameBox = browser.find_element_by_css_selector("#identifierId")
+            usernameBox.send_keys(username)
+            usernameBox.send_keys(Keys.ENTER)
+            time.sleep(5)
+    # password
+            passwordBox = browser.find_element_by_css_selector("#password > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > input:nth-child(1)")
+            passwordBox.send_keys(password)
+            passwordBox.send_keys(Keys.ENTER)
+            break
+        except:
+            browser.refresh()
+
     # joining the meeting
     browser.execute_script("window.open('');")
     browser.switch_to.window(browser.window_handles[2])
     browser.get(link)
-    time.sleep(5)
+    time.sleep(2)
     start = time.time()
-    time.sleep(5)
+    time.sleep(3)
     while True:
-        var = pyautogui.locateOnScreen('open.png', confidence=0.4)
+        var = pyautogui.locateOnScreen('open.png', confidence=0.6)
         if var != None:
             pyautogui.click(var)
             break
-        var = pyautogui.locateOnScreen('openzoom.png', confidence=0.4)
+        var = pyautogui.locateOnScreen('openzoom.png', confidence=0.6)
         if var != None:
             pyautogui.click(var)
             break
@@ -50,4 +61,4 @@ def JoinZoom(link, username, password):
             break
         time.sleep(3)
     return
-
+    
