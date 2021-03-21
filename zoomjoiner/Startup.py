5b95898ab6f9ac0e8959568c64785f52ab6cae0c
NKim123 <67401402+NKim123@users.noreply.github.com>
2021-03-20 23:04:27 -0400

    19

--- zoomjoiner/Startup.py
+++ zoomjoiner/Startup.py
@@ -1,10 +1,15 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("JoinPage.html")
+
+@app.route("/", method = ['GET'])
+def data(): 
+    if request.method == 'GET':
+        print('worked')
 
 if __name__=="__main__":
     app.run(debug=True)
