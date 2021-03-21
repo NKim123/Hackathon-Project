25d5a7dc566edd9096eaa993cf347925dad0fd18
noah kim <67401402+NKim123@users.noreply.github.com>
2021-03-21 09:43:24 -0400

    Update Startup.py

--- zoomjoiner/attempt1/Startup.py
+++ zoomjoiner/attempt1/Startup.py
@@ -1,13 +1,8 @@
 from flask import Flask, redirect, url_for, render_template, request
-<<<<<<< HEAD
-from selenium import Chrome
-
-=======
 from ChromeJoinZoom import JoinZoom
 from selenium.webdriver import Chrome
 from selenium import webdriver
 from Ringleader import Ringleader
->>>>>>> f1228e2b68a7e967c1e10bd883c2f9fd5e14d3c4
 app = Flask(__name__)
 userInfo = None
 
@@ -16,26 +11,19 @@ def home():
     return render_template("JoinPage.html")
 
 
-
-
 @app.route("/", methods = ['POST'])
 def data(): 
     if request.method == 'POST':
         print('worked')
         userInfo = request.form
-<<<<<<< HEAD
-        
-        return render_template("Success.html")
-=======
         print(userInfo)
         smile=Ringleader.prettywebpage()
         ringleader=Ringleader(userInfo)
-
-
+        return render_template("Success.html")
+        
 def smiles():
 	return render_template("Success.html");
         
->>>>>>> f1228e2b68a7e967c1e10bd883c2f9fd5e14d3c4
 
 if __name__=="__main__":
     app.run(debug=True)
